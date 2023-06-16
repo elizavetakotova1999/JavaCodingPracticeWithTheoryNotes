f406743d87609249912d2b0a29ecdd02092777f1
elizavetakotova1999 <kotakoray@gmail.com>
2023-06-16 03:25:48 +0300

    метод wait()

--- java/src/ObjectOrientedProgramming/ObjectClassMethods/Wait/QueueProcessor.java
+++ java/src/ObjectOrientedProgramming/ObjectClassMethods/Wait/QueueProcessor.java
@@ -1,9 +1,34 @@
-package ObjectOrientedProgramming.ObjectClassMethods.Wait.Processor;
+package ObjectOrientedProgramming.ObjectClassMethods.Wait;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+public class QueueProcessor implements Runnable {
+    private Queue<String> queue;
+
+    public QueueProcessor(Queue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        synchronized (queue) {
+            // Очікування, доки черга не стане доступною
+            while (queue.isEmpty()) {
+                try {
+                    queue.wait(); // Потік залишається у режимі очікування
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Виконання роботи з чергою
+            String data = queue.poll();
+            System.out.println("Оброблено елемент: " + data);
+        }
+    }
+
+
     public static void main(String[] args) {
         // Створення черги
         Queue<String> queue = new LinkedList<>();
